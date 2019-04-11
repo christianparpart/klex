887b0e941187eba5f17edfb39172de5c2b1cc0b2
Christian Parpart <christian@parpart.family>
2019-04-11 13:48:09 +0200

    example update

--- examples/mathexpr_ll.cc
+++ examples/mathexpr_ll.cc
@@ -14,8 +14,8 @@
 #include <klex/cfg/LeftRecursion.h>
 #include <klex/cfg/ll/Analyzer.h>
 
-#include <klex/util/Flags.h>
 #include <klex/Report.h>
+#include <klex/util/Flags.h>
 
 #include <fmt/format.h>
 #include <algorithm>
@@ -24,6 +24,10 @@
 #include <sstream>
 #include <stdexcept>
 #include <string_view>
+
+using namespace klex;
+using namespace klex::cfg;
+using namespace klex::cfg::ll;
 
 std::string_view const grammarSpec = R"(
 token {
@@ -61,7 +65,7 @@ int main(int argc, const char* argv[])
 	klex::cfg::GrammarParser grammarParser(std::string(grammarSpec), &report);
 	klex::cfg::Grammar g = grammarParser.parse();
 	klex::cfg::LeftRecursion{g}.direct();
-	g.finalize(); // ?
+	g.finalize();  // ?
 
 	std::cout << g.dump() << "\n";
 
@@ -72,12 +76,24 @@ int main(int argc, const char* argv[])
 	const std::string inputStr = "2 + 3 * 4";
 
 	using SemanticValue = int;
-	auto handler = [](int action, const klex::cfg::ll::Analyzer<SemanticValue>& analyzer) -> SemanticValue
-	{
-		return SemanticValue{42};
-	};
+	klex::cfg::ll::Analyzer<SemanticValue>::ActionHandlerMap actions = {
+		{st.actionId("add"),
+		 [](const Analyzer<SemanticValue>::Context&) -> SemanticValue {
+			 std::cout << fmt::format("call action add\n");
+			 return SemanticValue{42};
+		 }},
+		{st.actionId("mul"),
+		 [](const Analyzer<SemanticValue>::Context&) -> SemanticValue {
+			 std::cout << fmt::format("call action mul\n");
+			 return SemanticValue{42};
+		 }},
+		{st.actionId("num"),
+		 [](const Analyzer<SemanticValue>::Context&) -> SemanticValue {
+			 std::cout << fmt::format("call action num\n");
+			 return SemanticValue{42};
+		 }}};
 
-	klex::cfg::ll::Analyzer<SemanticValue> analyzer(st, &report, inputStr, handler);
+	klex::cfg::ll::Analyzer<SemanticValue> analyzer(st, &report, inputStr, actions);
 
 #if 1 == 0
 	analyzer.action("add", [](const auto& args) { return args[1] + args[2]; });
