48feb42130362f21ff49bc364d528114e37bb938
Christian Parpart <christian@parpart.family>
2019-04-11 14:39:27 +0200

    example overall code improvements and new api adaptions

--- examples/mathexpr_ll.cc
+++ examples/mathexpr_ll.cc
@@ -25,11 +25,12 @@
 #include <stdexcept>
 #include <string_view>
 
+using namespace std;
 using namespace klex;
 using namespace klex::cfg;
 using namespace klex::cfg::ll;
 
-std::string_view const grammarSpec = R"(
+string_view const grammarSpec = R"(
 token {
   Spacing(ignore) ::= [\s\t\n]+
   Number          ::= 0|[1-9][0-9]*
@@ -54,6 +55,8 @@ Factor    ::= Number             {num}
             ;
 )";
 
+using SemanticValue = int;
+
 int main(int argc, const char* argv[])
 {
 	auto flags = klex::util::Flags{};
@@ -61,59 +64,34 @@ int main(int argc, const char* argv[])
 	flags.enableParameters("EXPRESSION", "Mathematical expression to calculate");
 	flags.parse(argc, argv);
 
-	klex::ConsoleReport report;
-	klex::cfg::GrammarParser grammarParser(std::string(grammarSpec), &report);
-	klex::cfg::Grammar g = grammarParser.parse();
-	klex::cfg::LeftRecursion{g}.direct();
-	g.finalize();  // ?
+	auto report = klex::ConsoleReport{};
+	auto grammarParser = GrammarParser{string(grammarSpec), &report};
+	auto grammar = grammarParser.parse();
 
-	std::cout << g.dump() << "\n";
+	LeftRecursion{grammar}.direct();
+	grammar.finalize();
 
-	klex::cfg::ll::SyntaxTable st = klex::cfg::ll::SyntaxTable::construct(g);
+	cout << grammar.dump() << "\n";
 
-	st.dump(g);
+	auto const st = SyntaxTable::construct(grammar);
+	st.dump(grammar);
 
-	const std::string inputStr = "2 + 3 * 4";
+	auto const inputStr = string{"2 + 3 * 4"};
 
-	using SemanticValue = int;
-	klex::cfg::ll::Analyzer<SemanticValue>::ActionHandlerMap actions = {
-		{st.actionId("add"),
-		 [](const Analyzer<SemanticValue>::Context&) -> SemanticValue {
-			 std::cout << fmt::format("call action add\n");
-			 return SemanticValue{42};
-		 }},
-		{st.actionId("mul"),
-		 [](const Analyzer<SemanticValue>::Context&) -> SemanticValue {
-			 std::cout << fmt::format("call action mul\n");
-			 return SemanticValue{42};
-		 }},
-		{st.actionId("num"),
-		 [](const Analyzer<SemanticValue>::Context&) -> SemanticValue {
-			 std::cout << fmt::format("call action num\n");
-			 return SemanticValue{42};
-		 }}};
+	auto const actions = Analyzer<SemanticValue>::ActionNameMap{
+		{"add", [](const auto& args) { return args(-2) + args(-4); }},
+		{"mul", [](const auto& args) { return args(-2) * args(-4); }},
+		{"num", [](const auto& args) { return stoi(args.lastLiteral()); }},
+		{"var", [](const auto& args) { return 3.14; /* TODO */ }}
+	};
 
-	klex::cfg::ll::Analyzer<SemanticValue> analyzer(st, &report, inputStr, actions);
+	auto analyzer = Analyzer<SemanticValue>{st, &report, inputStr, actions};
 
-#if 1 == 0
-	analyzer.action("add", [](const auto& args) { return args[1] + args[2]; });
-	analyzer.action("sub", [](const auto& args) { return args[1] - args[2]; });
-	analyzer.action("mul", [](const auto& args) { return args[1] * args[2]; });
-	analyzer.action("div", [](const auto& args) { return args[1] / args[2]; });
-	analyzer.action("num", [](const auto& args) { return stoi(literal(args[1])); });
-	analyzer.action("var", [](const auto& args) {
-		if (literal(args[1]) == "pi")
-			return 3.14;
-		else
-			abort();
-	});
-#endif
-
-	std::optional<SemanticValue> result = analyzer.analyze();
+	optional<SemanticValue> result = analyzer.analyze();
 	if (result)
-		std::cout << "Result: " << *result << "\n";
+		cout << "Result: " << *result << "\n";
 	else
-		std::cout << "No Result.\n";
+		cout << "No Result.\n";
 
 	return EXIT_SUCCESS;
 }
