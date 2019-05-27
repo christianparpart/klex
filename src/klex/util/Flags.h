55cc7078b2c106881709e49dada3b2f92529387e
Christian Parpart <christian@parpart.family>
2019-05-28 00:21:58 +0200

    coding style updates

--- src/klex/util/Flags.h
+++ src/klex/util/Flags.h
@@ -7,183 +7,157 @@
 #pragma once
 
 #include <fmt/format.h>
-#include <optional>
 #include <functional>
+#include <list>
+#include <optional>
+#include <string>
 #include <system_error>
 #include <unordered_map>
-#include <vector>
-#include <list>
-#include <string>
 #include <utility>
+#include <vector>
 
 namespace klex::util {
 
 class Flags {
- public:
-  enum class FlagType {
-    String,
-    Number,
-    Float,
-    Bool,
-  };
+  public:
+    enum class FlagType {
+        String,
+        Number,
+        Float,
+        Bool,
+    };
 
-  // FlagPassingStyle
-  enum FlagStyle {
-    ShortSwitch,
-    LongSwitch,
-    ShortWithValue,
-    LongWithValue,
-    UnnamedParameter
-  };
+    // FlagPassingStyle
+    enum FlagStyle { ShortSwitch, LongSwitch, ShortWithValue, LongWithValue, UnnamedParameter };
 
-  enum class ErrorCode {
-    TypeMismatch,
-    UnknownOption,
-    MissingOption,
-    MissingOptionValue,
-    NotFound,
-  };
-  
-  class Error : public std::runtime_error {
-   public:
-    Error(ErrorCode code, std::string arg);
+    enum class ErrorCode {
+        TypeMismatch,
+        UnknownOption,
+        MissingOption,
+        MissingOptionValue,
+        NotFound,
+    };
 
-    ErrorCode code() const noexcept { return code_; }
-    const std::string& arg() const noexcept { return arg_; }
+    class Error : public std::runtime_error {
+      public:
+        Error(ErrorCode code, std::string arg);
 
-   private:
-    ErrorCode code_;
-    std::string arg_;
-  };
+        ErrorCode code() const noexcept { return code_; }
+        const std::string& arg() const noexcept { return arg_; }
 
-  struct FlagDef;
-  class Flag;
+      private:
+        ErrorCode code_;
+        std::string arg_;
+    };
 
-  Flags();
+    struct FlagDef;
+    class Flag;
 
-  std::string getString(const std::string& flag) const;
-  std::string asString(const std::string& flag) const;
-  long int getNumber(const std::string& flag) const;
-  float getFloat(const std::string& flag) const;
-  bool getBool(const std::string& flag) const;
+    Flags();
 
-  const std::vector<std::string>& parameters() const;
-  void setParameters(const std::vector<std::string>& v);
+    std::string getString(const std::string& flag) const;
+    std::string asString(const std::string& flag) const;
+    long int getNumber(const std::string& flag) const;
+    float getFloat(const std::string& flag) const;
+    bool getBool(const std::string& flag) const;
 
-  size_t size() const { return set_.size(); }
+    const std::vector<std::string>& parameters() const;
+    void setParameters(const std::vector<std::string>& v);
 
-  std::string to_s() const;
+    size_t size() const { return set_.size(); }
 
-  void set(const Flag& flag);
-  void set(const std::string& opt, const std::string& val, FlagStyle fs, FlagType ft);
-  bool isSet(const std::string& flag) const;
+    std::string to_s() const;
 
-  Flags& defineString(const std::string& longOpt,
-                      char shortOpt,
-                      const std::string& valuePlaceholder,
-                      const std::string& helpText,
-                      std::optional<std::string> defaultValue = std::nullopt,
-                      std::function<void(const std::string&)> callback = nullptr);
+    void set(const Flag& flag);
+    void set(const std::string& opt, const std::string& val, FlagStyle fs, FlagType ft);
+    bool isSet(const std::string& flag) const;
 
-  Flags& defineNumber(const std::string& longOpt,
-                      char shortOpt,
-                      const std::string& valuePlaceholder,
-                      const std::string& helpText,
-                      std::optional<long int> defaultValue = std::nullopt,
-                      std::function<void(long int)> callback = nullptr);
+    Flags& defineString(const std::string& longOpt, char shortOpt, const std::string& valuePlaceholder,
+                        const std::string& helpText, std::optional<std::string> defaultValue = std::nullopt,
+                        std::function<void(const std::string&)> callback = nullptr);
 
-  Flags& defineFloat(const std::string& longOpt,
-                     char shortOpt,
-                     const std::string& valuePlaceholder,
-                     const std::string& helpText,
-                     std::optional<float> defaultValue = std::nullopt,
-                     std::function<void(float)> callback = nullptr);
+    Flags& defineNumber(const std::string& longOpt, char shortOpt, const std::string& valuePlaceholder,
+                        const std::string& helpText, std::optional<long int> defaultValue = std::nullopt,
+                        std::function<void(long int)> callback = nullptr);
 
-  Flags& defineBool(const std::string& longOpt,
-                    char shortOpt,
-                    const std::string& helpText,
-                    std::function<void(bool)> callback = nullptr);
+    Flags& defineFloat(const std::string& longOpt, char shortOpt, const std::string& valuePlaceholder,
+                       const std::string& helpText, std::optional<float> defaultValue = std::nullopt,
+                       std::function<void(float)> callback = nullptr);
 
-  Flags& enableParameters(const std::string& valuePlaceholder,
-                          const std::string& helpText);
+    Flags& defineBool(const std::string& longOpt, char shortOpt, const std::string& helpText,
+                      std::function<void(bool)> callback = nullptr);
 
-  std::string helpText() const { return helpText(78, 30); }
-  std::string helpText(size_t width, size_t helpTextOffset) const;
+    Flags& enableParameters(const std::string& valuePlaceholder, const std::string& helpText);
 
-  const FlagDef* findDef(const std::string& longOption) const;
-  const FlagDef* findDef(char shortOption) const;
+    std::string helpText() const { return helpText(78, 30); }
+    std::string helpText(size_t width, size_t helpTextOffset) const;
 
-  void parse(int argc, const char* argv[]);
-  void parse(const std::vector<std::string>& args);
+    const FlagDef* findDef(const std::string& longOption) const;
+    const FlagDef* findDef(char shortOption) const;
 
-  // Attempts to parse given arguments and returns an error code in case of parsing errors instead
-  // of throwing.
-  std::error_code tryParse(const std::vector<std::string>& args);
+    void parse(int argc, const char* argv[]);
+    void parse(const std::vector<std::string>& args);
 
- private:
-  Flags& define(const std::string& longOpt,
-                char shortOpt,
-                bool required,
-                FlagType type,
-                const std::string& helpText,
-                const std::string& valuePlaceholder,
-                const std::optional<std::string>& defaultValue,
-                std::function<void(const std::string&)> callback);
+    // Attempts to parse given arguments and returns an error code in case of parsing errors instead
+    // of throwing.
+    std::error_code tryParse(const std::vector<std::string>& args);
 
- private:
-  std::list<FlagDef> flagDefs_;
-  bool parametersEnabled_;              // non-option parameters enabled?
-  std::string parametersPlaceholder_;
-  std::string parametersHelpText_;
+  private:
+    Flags& define(const std::string& longOpt, char shortOpt, bool required, FlagType type,
+                  const std::string& helpText, const std::string& valuePlaceholder,
+                  const std::optional<std::string>& defaultValue,
+                  std::function<void(const std::string&)> callback);
 
-  typedef std::pair<FlagType, std::string> FlagValue;
-  std::unordered_map<std::string, FlagValue> set_;
-  std::vector<std::string> raw_;
+  private:
+    std::list<FlagDef> flagDefs_;
+    bool parametersEnabled_;  // non-option parameters enabled?
+    std::string parametersPlaceholder_;
+    std::string parametersHelpText_;
+
+    typedef std::pair<FlagType, std::string> FlagValue;
+    std::unordered_map<std::string, FlagValue> set_;
+    std::vector<std::string> raw_;
 };
 
 struct Flags::FlagDef {
-  FlagType type;
-  std::string longOption;
-  char shortOption;
-  bool required;
-  std::string valuePlaceholder;
-  std::string helpText;
-  std::optional<std::string> defaultValue;
-  std::function<void(const std::string&)> callback;
+    FlagType type;
+    std::string longOption;
+    char shortOption;
+    bool required;
+    std::string valuePlaceholder;
+    std::string helpText;
+    std::optional<std::string> defaultValue;
+    std::function<void(const std::string&)> callback;
 
-  std::string makeHelpText(size_t width, size_t helpTextOffset) const;
+    std::string makeHelpText(size_t width, size_t helpTextOffset) const;
 };
 
 class Flags::Flag {
- public:
-  Flag(
-      const std::string& opt,
-      const std::string& val,
-      FlagStyle fs,
-      FlagType ft);
+  public:
+    Flag(const std::string& opt, const std::string& val, FlagStyle fs, FlagType ft);
 
-  explicit Flag(char shortOpt);
-  Flag(char shortOpt, const std::string& val);
-  Flag(const std::string& longOpt);
-  Flag(const std::string& longOpt, const std::string& val);
+    explicit Flag(char shortOpt);
+    Flag(char shortOpt, const std::string& val);
+    Flag(const std::string& longOpt);
+    Flag(const std::string& longOpt, const std::string& val);
 
-  FlagType type() const { return type_; }
-  const std::string& name() const { return name_; }
-  const std::string& value() const { return value_; }
+    FlagType type() const { return type_; }
+    const std::string& name() const { return name_; }
+    const std::string& value() const { return value_; }
 
- private:
-  FlagType type_;
-  FlagStyle style_;
-  std::string name_;
-  std::string value_;
+  private:
+    FlagType type_;
+    FlagStyle style_;
+    std::string name_;
+    std::string value_;
 };
 
 class FlagsErrorCategory : public std::error_category {
- public:
-  static FlagsErrorCategory& get();
+  public:
+    static FlagsErrorCategory& get();
 
-  const char* name() const noexcept override;
-  std::string message(int ec) const override;
+    const char* name() const noexcept override;
+    std::string message(int ec) const override;
 };
 
 std::error_code make_error_code(Flags::ErrorCode errc);
@@ -191,18 +165,24 @@ std::error_code make_error_code(Flags::ErrorCode errc);
 }  // namespace klex::util
 
 namespace std {
-  template<> struct is_error_code_enum<klex::util::Flags::ErrorCode> : public std::true_type {};
-} // namespace std
+template <>
+struct is_error_code_enum<klex::util::Flags::ErrorCode> : public std::true_type {
+};
+}  // namespace std
 
 namespace fmt {
-  template<>
-  struct formatter<klex::util::Flags> {
+template <>
+struct formatter<klex::util::Flags> {
     template <typename ParseContext>
-    constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
+    constexpr auto parse(ParseContext& ctx)
+    {
+        return ctx.begin();
+    }
 
     template <typename FormatContext>
-    constexpr auto format(const klex::util::Flags& v, FormatContext &ctx) {
-      return format_to(ctx.out(), v.to_s());
+    constexpr auto format(const klex::util::Flags& v, FormatContext& ctx)
+    {
+        return format_to(ctx.out(), v.to_s());
     }
-  };
-}
+};
+}  // namespace fmt
